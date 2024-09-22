de0c80105b95de7d85be933cdbe72e018f1e4a5c
joymajumder23 <joymajumderjishu@gmail.com>
2024-09-22 23:33:57 +0600

    tabs adding

--- src/components/Home/Home/Home.jsx
+++ src/components/Home/Home/Home.jsx
@@ -3,12 +3,19 @@ import icon1 from "../../../assets/images/icon2.png";
 import icon2 from "../../../assets/images/icon6.png";
 import icon3 from "../../../assets/images/icon3.png";
 import icon4 from "../../../assets/images/icon1.png";
+import Tabs from "../Tabs/Tabs";
 const Home = () => {
     return (
         <div className="mx-auto max-w-screen-xl">
             <div className="flex justify-between">
                 <div>
                     <h3 className="flex items-center gap-2 text-2xl font-semibold"><img className="w-9" src={icon1} alt="" />Bitcoin <span className="text-slate-400">BTC</span></h3>
+
+                    <section>
+                        <div>
+                            <Tabs></Tabs>
+                        </div>
+                    </section>
                 </div>
                 <div>
                     <div className="bg-[#0052FE] px-8 py-4 rounded-2xl w-[426px] h-[515px]flex justify-center text-center space-y-6">
@@ -22,8 +29,8 @@ const Home = () => {
                         </div>
                         <button className="btn bg-white">Get Started for FREE</button>
                     </div>
-                    <div>
-                        <h1>Trending Coins (24h)</h1>
+                    <div className="space-y-6 bg-white p-6 rounded">
+                        <h1 className="text-2xl font-semibold">Trending Coins (24h)</h1>
                         <div className="flex gap-36 items-center">
                             <div className="space-y-4">
                                 <div className="flex items-center gap-1">
---
+++ src/components/Home/Tabs/Tabs.jsx
@@ -0,0 +1,32 @@
+const Tabs = () => {
+    return (
+        <div>
+            <div role="tablist" className="tabs tabs-bordered">
+                <input type="radio" name="my_tabs_1" role="tab" className="tab" aria-label="Overview" />
+                <div role="tabpanel" className="tab-content p-10">Overview</div>
+
+                <input
+                    type="radio"
+                    name="my_tabs_1"
+                    role="tab"
+                    className="tab"
+                    aria-label="Fundamentals"
+                    defaultChecked />
+                <div role="tabpanel" className="tab-content p-10">Fundamentals</div>
+
+                <input type="radio" name="my_tabs_1" role="tab" className="tab" aria-label="News Insights" />
+                <div role="tabpanel" className="tab-content p-10">News Insights</div>
+                <input type="radio" name="my_tabs_1" role="tab" className="tab" aria-label="Sentiments" />
+                <div role="tabpanel" className="tab-content p-10">Sentiments</div>
+                <input type="radio" name="my_tabs_1" role="tab" className="tab" aria-label="Team" />
+                <div role="tabpanel" className="tab-content p-10">Team</div>
+                <input type="radio" name="my_tabs_1" role="tab" className="tab" aria-label="Technicals" />
+                <div role="tabpanel" className="tab-content p-10">Technicals</div>
+                <input type="radio" name="my_tabs_1" role="tab" className="tab" aria-label="Tokenomics" />
+                <div role="tabpanel" className="tab-content p-10">Tokenomics</div>
+            </div>
+        </div>
+    );
+};
+
+export default Tabs;
